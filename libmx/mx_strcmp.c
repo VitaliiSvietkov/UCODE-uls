55b588734bb4b0e876ae5ed01c741c52e685e45b
VitaliiSvietkov <a1vitalii.sv@gmail.com>
2020-10-31 19:50:54 +0200

    Fixed mx_strcmp.c

--- libmx/mx_strcmp.c
+++ libmx/mx_strcmp.c
@@ -21,8 +21,18 @@ int mx_strcmp(const char *s1, const char *s2) {
     int len1 = mx_strlen(s1);
     int len2 = mx_strlen(s2);
     for (int i = 0; i < len1 && i < len2; i++) {
-        if (s1[i] != s2[i])
-            return s1[i] - s2[i];
+        if (s1[i] != s2[i]) {
+            char c1 = s1[i];
+            char c2 = s2[i];
+            if (mx_isalpha(c1) && mx_isalpha(c2)) {
+                if (mx_isupper(c1))
+                    c1 += 32;
+                if (mx_isupper(c2))
+                    c2 += 32;
+                return c1 - c2;
+            }
+            return c1 - c2;
+        }
     }
     if (len1 < len2)
         return -1;
