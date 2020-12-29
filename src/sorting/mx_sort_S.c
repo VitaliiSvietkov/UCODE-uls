e1a3325b646419aa57ed5579dc0c4ecde110be03
VitaliiSvietkov <a1vitalii.sv@gmail.com>
2020-12-29 23:11:10 +0200

    Fixed sorting with -S but it should be checked

--- src/sorting/mx_sort_S.c
+++ src/sorting/mx_sort_S.c
@@ -8,7 +8,7 @@ void mx_sort_S(char **arr, char *dir_path, t_options *opts) {
     struct stat buf1;
     struct stat buf2;
 
-    for (int i = 0; i < len - 1; i++) {
+    /*for (int i = 0; i < len - 1; i++) {
         char *path1 = mx_strjoin(dir_path, arr[i]);
         lstat(path1, &buf1);
         free(path1);
@@ -27,6 +27,27 @@ void mx_sort_S(char **arr, char *dir_path, t_options *opts) {
                 arr[j] = temp;
             }
         }
+    }*/
+
+    for (int i = 0; i < len - 1; i++) {
+        char *path1 = mx_strjoin(dir_path, arr[i]);
+        lstat(path1, &buf1);
+        free(path1);
+        int max_index = i;
+        for (int j = i + 1; j < len; j++) {
+            char *path2 = mx_strjoin(dir_path, arr[j]);
+            lstat(path2, &buf2);
+            free(path2);
+            if (buf1.st_size < buf2.st_size) {
+                max_index = j;
+            }
+            /*else if (mx_strcmp(arr[i], arr[j]) > 0) {
+                max_index = j;
+            }*/
+        }
+        char *temp = arr[i];
+        arr[i] = arr[max_index];
+        arr[max_index] = temp;
     }
 
     if (opts->using_r)
